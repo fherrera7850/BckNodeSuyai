5a48975be1aba80c1054b40d4995f1854c4e2177
fherrera7850 <fherrera7850@gmail.com>
2022-10-06 11:02:46 -0300

    db

--- src/database/database.js
+++ src/database/database.js
@@ -1,12 +1,15 @@
 import mysql from "promise-mysql";
 import config from "./../config";
 
-const connection = mysql.createConnection({
+const parameters = {
     host: config.host,
     database: config.database,
     user: config.user,
     password: config.password
-});
+
+}
+console.log("🚀 ~ file: database.js ~ line 11 ~ parameters", parameters)
+const connection = mysql.createConnection(parameters);
 
 const getConnection = () => {
     return connection;
