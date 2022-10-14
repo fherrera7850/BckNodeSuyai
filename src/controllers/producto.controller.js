b16e86c0e1aff1c1202159cf78a2ebd837e65857
fherrera7850 <fherrera7850@gmail.com>
2022-10-14 11:19:55 -0300

    Observacion, loader y sort por mas vendidos

--- src/controllers/producto.controller.js
+++ src/controllers/producto.controller.js
@@ -3,7 +3,12 @@ import { getConnection } from "./../database/database";
 const getProductos = async (req, res) => {
     try {
         const connection = await getConnection();
-        const result = await connection.query("SELECT * FROM producto");
+        let qry = "select p.*, count(pv.Producto_id) as Ventas "
+        qry += "from producto p left join productoventa pv "
+        qry += "on p._id=pv.Producto_id "
+        qry += "GROUP by p._id "
+        qry += "order by ventas desc"
+        const result = await connection.query(qry);
         res.json(result);
     } catch (error) {
         res.status(500);
