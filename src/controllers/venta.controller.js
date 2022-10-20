91161c6b88082174bf3424bac9d0ce0fef6efcdc
fherrera7850 <fherrera7850@gmail.com>
2022-10-20 11:17:23 -0300

    estadisticas ganancia,productos y medios de pago

--- src/controllers/venta.controller.js
+++ src/controllers/venta.controller.js
@@ -8,7 +8,7 @@ const addVenta = async (req, res) => {
     fx = fx.replace(/Z/g, "")
     Venta.Fecha = fx
     console.log("🚀 ~ file: venta.controller.js ~ line 8 ~ addVenta ~ fx", fx)
-    
+
 
     const connection = await getConnection();
 
@@ -45,7 +45,7 @@ const getHistorial30Dias = async (req, res) => {
         qry += 'WHERE fecha >= DATE_SUB(CURDATE(), INTERVAL 30 day) '
         qry += 'GROUP by FechaVenta '
         qry += 'order by FechaVenta desc'
-        console.log("🚀 ~ file: venta.controller.js ~ line 48 ~ getHistorial30Dias ~ qry", qry)
+        //console.log("🚀 ~ file: venta.controller.js ~ line 48 ~ getHistorial30Dias ~ qry", qry)
 
         const resultAgrupados = await connection.query(qry);
 
@@ -57,7 +57,7 @@ const getHistorial30Dias = async (req, res) => {
             qry2 += 'WHERE venta.Cliente_id=cliente._id and '
             qry2 += 'fecha >= DATE_SUB(CURDATE(), INTERVAL 30 day) '
             qry2 += 'order by fecha desc'
-            console.log("🚀 ~ file: venta.controller.js ~ line 60 ~ getHistorial30Dias ~ qry2", qry2)
+            //console.log("🚀 ~ file: venta.controller.js ~ line 60 ~ getHistorial30Dias ~ qry2", qry2)
 
             const resultVentas = await connection.query(qry2);
 
@@ -65,24 +65,15 @@ const getHistorial30Dias = async (req, res) => {
 
             resultAgrupados.forEach(parent => {
                 resultVentas.forEach(child => {
-                    let fecha1 = new Date(parent.FechaVenta).toLocaleDateString()
-                    
-                    let fecha2 = new Date(child.Fecha).toLocaleDateString()
-                    //console.log("🚀 ~ file: venta.controller.js ~ line 72 ~ getHistorial30Dias ~ child.Fecha.split(T)", child.Fecha.split("T")[0])
-                    //console.log("fechaFormateada", fecha1, fecha2)
                     if (parent.FechaVenta === child.Fecha.split("T")[0]) {
-                        
-                        
-                        //console.log("iguales", fecha1, fecha2)
                         parent.Ventas.push(child)
                     }
                 });
             });
-            //console.log(resultAgrupados)
             res.json(resultAgrupados);
         }
         else {
-            res.json({ErrorMessage: "No hay ventas para mostrar"})
+            res.json({ ErrorMessage: "No hay ventas para mostrar" })
         }
 
 
@@ -95,31 +86,60 @@ const getHistorial30Dias = async (req, res) => {
 
 const getEstadisticas = async (req, res) => {
     try {
+
         const connection = await getConnection();
-        let qry = "SELECT count(_id) NroVentas, COALESCE(sum(PrecioTotalVenta), 0) SumaVentas "
-        qry += "FROM venta "
-        qry += "where DATE_SUB(Fecha,INTERVAL 3 HOUR) BETWEEN '" + req.params.FechaInicio + " 00:00:00' AND '" + req.params.FechaFin + " 23:59:00';"
-        
-        //qry += "where Fecha BETWEEN '" + req.params.FechaInicio + " 00:00:00' AND '" + req.params.FechaFin + " 23:59:00';"
-        
-        const result = await connection.query(qry);
-        res.json(result);
+
+        let qryTotales = "SELECT "
+        qryTotales += "count(_id) NroVentas, "
+        qryTotales += "COALESCE(sum(PrecioTotalVenta), 0) SumaVentas, "
+        qryTotales += "avg(preciototalventa) PromedioVentas, "
+        qryTotales += "GananciaVentas "
+        qryTotales += "FROM venta, (select (sum(pv.PrecioVentaProducto*pv.cantidad) - sum(p.Costo*pv.cantidad)) GananciaVentas "
+        qryTotales += "from productoventa pv inner join producto p "
+        qryTotales += "on pv.Producto_id=p._id inner join venta v "
+        qryTotales += "on v._id=pv.Venta_id "
+        qryTotales += "where "
+        qryTotales += "DATE_SUB(v.Fecha,INTERVAL 3 HOUR) BETWEEN '" + req.params.FechaInicio + " 00:00:00' AND '" + req.params.FechaFin + " 23:59:59') temp2 where  "
+        qryTotales += "DATE_SUB(Fecha,INTERVAL 3 HOUR) BETWEEN '" + req.params.FechaInicio + " 00:00:00' AND '" + req.params.FechaFin + " 23:59:59'"
+
+        const resultTotales = await connection.query(qryTotales);
+        console.log("🚀 ~  resultTotales", resultTotales)
+
+
+        let qryMediosDePago = "select "
+        qryMediosDePago += "CASE "
+        qryMediosDePago += "WHEN mediopago = 0 THEN 'Efectivo' "
+        qryMediosDePago += "WHEN mediopago = 1 THEN 'Transferencia' "
+        qryMediosDePago += "WHEN mediopago = 2 THEN 'Tarjeta' "
+        qryMediosDePago += "ELSE '-' "
+        qryMediosDePago += "END mediopago, count(mediopago) cantidad "
+        qryMediosDePago += "from venta "
+        qryMediosDePago += "group by mediopago order by cantidad desc;"
+
+        const resultMediosDePago = await connection.query(qryMediosDePago);
+        console.log("🚀 ~  resultMediosDePago", resultMediosDePago)
+
+        resultTotales[0].MediosDePago = resultMediosDePago
+
+        let qryMasVendidos = "select p.nombre, sum(cantidad) cantidad,sum(pv.PrecioVentaProducto*pv.Cantidad) total "
+        qryMasVendidos += "from productoventa pv inner join producto p "
+        qryMasVendidos += "on p._id=pv.Producto_id "
+        qryMasVendidos += "group by pv.producto_id "
+        qryMasVendidos += "order by cantidad desc;"
+
+        const resultMasVendidos = await connection.query(qryMasVendidos);
+        console.log("🚀 ~ resultMasVendidos", resultMasVendidos)
+
+        resultTotales[0].MasVendidos = resultMasVendidos
+
+        //const result = await connection.query(qryTotales);
+        res.json(resultTotales);
+
     } catch (error) {
         res.status(500);
         res.send(error.message);
     }
 };
-
-/* 
-SELECT count(_id) NroVentas, sum(PrecioTotalVenta) SumaVentas
-FROM `venta`
-where Fecha BETWEEN '2022-09-29 00:00:00' AND '2022-10-03 23:59:00'; */
-
-//SELECT count(_id) as NroVentas,sum(Preciototalventa) as SumaVentas, DATE_FORMAT(fecha, " %d-%m-%Y") as FechaVenta 
-//from Venta 
-//WHERE fecha >= DATE_SUB(CURDATE(), INTERVAL 30 day) 
-//GROUP by day(fecha) 
-//order by fecha desc
 
 export const methods = {
     addVenta,
