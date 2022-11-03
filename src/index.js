ab2ac809cf47deef72401a7efc22d289df02e2e7
fherrera7850 <fherrera7850@gmail.com>
2022-11-03 17:20:56 -0300

    ver env

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import app from "./app";
 
 const main = () => {
+    console.log(process.env.HOST, process.env.PORT, process.env.DBPORT, process.env.USER)
     app.listen(app.get("port"));
     console.log(`Server on port ${app.get("port")}`);
 };
